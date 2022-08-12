965dc05345c34c92a35a66aeff4334d6c89d174c
Ananya <52604742+ananyagandhi01@users.noreply.github.com>
2022-08-12 00:03:28 -0400

    updated to add spending data

--- spending.py
+++ spending.py
@@ -4,10 +4,11 @@
 import pandas as pd
 import numpy as np
 from datetime import datetime
-import plotly.express as px
-import plotly.graph_objecst as go
-from jupyter_dash import JupyterDash
-import dash_core_components as dcc
-import dash_html_components as html
+#import plotly.express as px
+#import plotly.graph_objecst as go
+#from jupyter_dash import JupyterDash
+#import dash_core_components as dcc
+#import dash_html_components as html
 
-
+venmo = pd.read_csv('data/merged-venmo.csv')
+print(venmo.head())
